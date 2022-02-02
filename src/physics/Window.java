4cb7396334be99cf487fbd4da3b2fd25c8e8c6fc
dafuce <dafuce@gmail.com>
2022-02-02 21:03:54 +0100

    just checking

--- src/physics/Window.java
+++ src/physics/Window.java
@@ -10,6 +10,27 @@ public class Window extends JFrame {
 
     public Graphics g;
 
+
+    private final char[][] stage =
+            {
+                    {'b','b','b','b','b'},
+                    {'b','s','b','f','b'},
+                    {'b','e','b','e','b'},
+                    {'b','e','e','e','b'},
+                    {'b','b','b','b','b'}
+            };
+    public static int NROWS;
+    public static int NCOLS;
+
+    private final int tileWidth;
+    private final int tileHeight;
+
+    /*
+    Values: 'e' = empty
+            'b' = wall block
+            's' = start
+            'f' = finish
+     */
     Window(){
         setTitle("Physics Demo");
         setSize(WIDTH, HEIGHT);
@@ -22,5 +43,36 @@ public class Window extends JFrame {
         panel.setBackground(Color.BLACK);
         Graphics g = getGraphics();
 
+        NROWS = stage.length;
+        NCOLS = stage[0].length;
+
+        tileWidth = WIDTH/NCOLS;
+        tileHeight = HEIGHT/NROWS;
+
+        System.out.println(NCOLS);
+
+    }
+    public void paint(Graphics g){
+        super.paint(g);
+
+        for(int row = 0; row < NROWS; row++){
+            for (int col = 0; col < NCOLS; col++){
+                Color color;
+                switch (stage[row][col]){
+                    case 'b': color = Color.BLACK;
+                        break;
+                    case 's': color = Color.YELLOW;
+                        break;
+                    case 'f': color = Color.GREEN;
+                        break;
+                    default:
+                        color = Color.WHITE;
+                }
+                g.setColor(color);
+                g.fillRect(tileWidth*col, tileHeight*row, tileWidth,tileHeight);
+                g.setColor(Color.BLACK);
+                g.drawRect(tileWidth*col, tileHeight*row, tileWidth,tileHeight);
+            }
+        }
     }
 }
